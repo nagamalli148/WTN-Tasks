8ac1bec23a370ee2f4321f9c91f58e6359dd48fe
Naga Malleswari Chennamsetti <60144643+nagamalli148@users.noreply.github.com>
2020-07-30 00:46:56 +0530

    Student Grade Management System

---
+++ Student Grade Calculation/NullMarksArrayException.java
@@ -0,0 +1,12 @@
+package com.mile1.exception;
+
+@SuppressWarnings("serial")
+public class NullMarksArrayException extends Exception{
+
+	@Override
+	public String toString() {
+		return "Marks array is null";
+	}
+	
+
+}
